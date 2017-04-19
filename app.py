0c27aa5a9c4d976fae8547cdc21e298f59fb142c
Siddharth Sahay <sahaysid@gmail.com>
2017-04-19 08:03:50 -0400

    Correcting reply no error message

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import AfterTheDeadlineHelper as ATD
 
 app = Flask("apiai-grammarchecker")
 
-replies_no_error = ["There seems to be no problem with this!", \
+reply_no_errors = ["There seems to be no problem with this!", \
 		"I think this is perfectly alright!", \
 		"I don't think there is any problem with what you have there!", \
 		"It's all correct!"]
@@ -39,7 +39,7 @@ def getWrongSentence(request):
 def getResponse(wrong_sentence):
 	response = ATD.checkDocument(wrong_sentence)
 	if response.errorCount == 0:
-		return replies_no_error[random.randint(0, len(replies_no_errors)-1]
+		return reply_no_errors[random.randint(0, len(replies_no_errors)-1)]
 
 	for e in range(response.errorCount):
 		urltext = response.getURLText(e)
